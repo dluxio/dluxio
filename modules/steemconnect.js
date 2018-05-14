1026c73c8b3841e21af4b966bcd74b520cc668be
Dluxio <sjettingerjr@gmail.com>
2018-05-13 22:57:28 -0700

    iterate
    iterate

--- modules/steemconnect.js
+++ modules/steemconnect.js
@@ -4,7 +4,7 @@ let config = require('../config')
 let steem = steemconnect2.Initialize({
     app: config.auth.client_id,
     callbackURL: config.auth.redirect_uri ,
-    scope: ['login','vote', 'comment']
+    scope: ['login','vote','comment','comment_options']
 });
 
 module.exports = steem;

83db8acafde3d839a1b63f04404bfe71f4e5df9d
Steven Ettinger <sjettingerjr@gmail.com>
2018-06-10 21:05:11 -0700

    external redirect after login
    Stores ref post in cookie for return after login

--- public/js/nav.js
+++ public/js/nav.js
@@ -534,3 +534,26 @@ AFRAME.registerComponent('set-camera', {
       }
     }
   });
+
+  function setCookie(cname, cvalue, exmins) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exmins*60*1000));
+      var expires = "expires="+ d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i <ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+              c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+              return c.substring(name.length, c.length);
+          }
+      }
+      return "";
+  }

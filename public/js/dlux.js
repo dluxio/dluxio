b66ada58a3e56af1e4762898162e2944f9a40312
markegiles <mark@mgcine.com>
2018-07-15 17:01:48 -0700

    added custom controls

--- public/js/dlux.js
+++ public/js/dlux.js
@@ -1,3 +1,28 @@
+AFRAME.registerComponent('custom-controls', {
+  schema: {
+    hand: {default: ''},
+    model: {default: 'customControllerModel.gltf'}
+  },
+
+  update: function () {
+    var hand = this.data.hand;
+    var controlConfiguration = {
+      hand: hand,
+      model: false,
+      orientationOffset: {x: 0, y: 0, z: hand === 'left' ? 90 : -90}
+    };
+
+    // Build on top of controller components.
+    el.setAttribute('vive-controls', controlConfiguration);
+    el.setAttribute('oculus-touch-controls', controlConfiguration);
+    el.setAttribute('daydream-controls', controlConfiguration);
+    el.setAttribute('gearvr-controls', controlConfiguration);
+    el.setAttribute('windows-motion-controls', controlConfiguration);
+
+    // Set a model.
+    el.setAttribute('gltf-model', this.data.model);
+  }
+});
 // Target URL
 AFRAME.registerComponent('url', {
   schema: {default: ''},

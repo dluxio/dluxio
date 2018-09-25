fc57726af8ee8050a9cb297845212b0806220248
Unknown <sjettingerjr@gmail.com>
2018-09-24 22:55:24 -0700

    custom redirects
    marketing

--- routes/index.js
+++ routes/index.js
@@ -86,6 +86,15 @@ router.get('/@:username', (req, res, next) => {
         userfeed: username,
       });
 });
+router.get('/p/mark', (req, res, next) => {
+      res.redirect('/dluxar/@dlux-io/tydl2qzlcimah1bjhm7gw9n0ev6f9yhz');
+});
+router.get('/p/steve', (req, res, next) => {
+      res.redirect('/dluxar/@dlux-io/tydl2qzlcimah1bjhm7gw9n0ev6f9yhz');
+});
+router.get('/p/esther', (req, res, next) => {
+      res.redirect('/dluxar/@dlux-io/tydl2qzlcimah1bjhm7gw9n0ev6f9yhz');
+});
 
 /* GET a users blog feed page. */
 router.get('/@:username/feed', (req, res, next) => {

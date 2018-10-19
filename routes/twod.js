18e7f8a3859c9672279034948d1d9b6f371d4c74
Unknown <sjettingerjr@gmail.com>
2018-10-19 16:20:02 -0700

    working on it

--- routes/twod.js
+++ routes/twod.js
@@ -5,54 +5,89 @@ let router = express.Router();
 /* GET a feed page. */
 router.get('/@:username', (req, res, next) => {
       let username = req.params.username
-      if (!req.user) {
-        let iAm = 'Guest'
-      } else {
+      if (req.user) {
         let iAm = req.user.username
+        renderU();
+      } else {
+        renderG();
       }
+      function renderG() {
       res.render('profile2d', {
-        user: iAm,
         username: username,
       });
+    }
+    function renderU() {
+    res.render('profile2d', {
+      user: iAm,
+      username: username,
+    });
+  }
 });
 router.get('/:feed/:tag?', (req, res, next) => {
-  if (!req.user) {
-    let iAm = 'Guest'
-  } else {
-    let iAm = req.user.username
-  }
     let feed = req.params.feed
     let tag = req.params.tag
+    if (req.user) {
+      let iAm = req.user.username
+      renderU();
+    } else {
+      renderG();
+    }
+    function renderG() {
     res.render('feed2d', {
-      user: iAm,
       feed: feed,
       tag: tag
     });
+  }
+  function renderU() {
+  res.render('feed2d', {
+    user: iAm,
+    feed: feed,
+    tag: tag
+  });
+}
 });
 router.get('/@:username/feed', (req, res, next) => {
-    if (!req.user) {
-      let iAm = 'Guest'
-    } else {
-      let iAm = req.user.username
-    }
     let username = req.params.username
+    if (req.user) {
+      let iAm = req.user.username
+      renderU();
+    } else {
+      renderG();
+    }
+    function renderG() {
+    res.render('feed2d', {
+      feed: 'user-feed',
+      username: username
+    });
+  }
+    function renderU() {
     res.render('feed2d', {
       user: iAm,
       feed: 'user-feed',
       username: username
     });
+  }
 });
 router.get('/', (req, res, next) =>  {
-  if (!req.user) {
-    let iAm = 'Guest'
-  } else {
+  if (req.user) {
     let iAm = req.user.username
+    renderU();
+  } else {
+    renderG();
   }
+  function renderG() {
+  res.render('feed2d', {
+    feed: 'feed',
+    tag: 'dlux'
+  });
+}
+  function renderU() {
   res.render('feed2d', {
     user: iAm,
     feed: 'feed',
     tag: 'dlux'
   });
+}
 });
 
 module.exports = router;
